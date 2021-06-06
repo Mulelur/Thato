37dcfb058ea5b748dc3177cb5401ec79fe481beb
rotonda <rotondwamulelu@gmail.com>
2021-06-06 14:04:02 +0200

    Init

---
+++ src/container/contact/contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Contact } from "../../components";
+import { Title } from "../../components/header/styles/header";
+import { LocationIcon, MailIcon, PhoneIcon } from "../../icons/icons";
+
+export default function ContactContainer() {
+  return (
+    <Contact>
+      <Contact.Header>
+        <Contact.Title>Contact Me</Contact.Title>
+        <Contact.Text></Contact.Text>
+      </Contact.Header>
+      <Contact.Body>
+        <Contact.Icons>
+          <Contact.IconsItem>
+            <PhoneIcon />
+            <Contact.IconsTitle>Call Me On</Contact.IconsTitle>
+            <Contact.Text>+27 81 595 3821</Contact.Text>
+          </Contact.IconsItem>
+          <Contact.IconsItem>
+            <MailIcon />
+            <Contact.IconsTitle>Email Me At</Contact.IconsTitle>
+            <Contact.Text email>thatomzangwa09@gmail.com </Contact.Text>
+          </Contact.IconsItem>
+          <Contact.IconsItem>
+            <LocationIcon />
+            <Contact.IconsTitle>Visit Me</Contact.IconsTitle>
+            <Contact.Text>
+              15562 Iraq Gorten Zamdela Sasolburg 1947
+            </Contact.Text>
+          </Contact.IconsItem>
+        </Contact.Icons>
+      </Contact.Body>
+    </Contact>
+  );
+}
